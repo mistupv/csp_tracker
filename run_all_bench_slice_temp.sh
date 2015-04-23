70c06b1001798bbf223c98434929a8e8c547bc05
Salvador Tamarit <tamarit27@gmail.com>
2015-04-23 16:44:46 +0200

    temporal file to run remaining benchmarks

---
+++ run_all_bench_slice_temp.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+array=("Loop" "Disk" "Oven" "ProdCons" "ReadWrite" "Traffic" "ABP" "ATM" "CPU")
+for i in "${array[@]}"; do 
+    escript run_bench.sh 'benchmarks/'$i'Slice.csp' > results/$i.txt
+done
+
