f78329e5f2b614072ba01ddb4fb8f8bb2c0bd381
Salvador Tamarit <tamarit27@gmail.com>
2015-04-24 09:57:30 +0200

    updated temporal benchmark script

--- run_all_bench_slice_temp.sh
+++ run_all_bench_slice_temp.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-array=("ProdCons" "ReadWrite" "Traffic" "ABP" "ATM" "CPU" "Oven" )
+array=("ATM" "CPU" "Oven" )
 for i in "${array[@]}"; do 
     escript run_bench.sh 'benchmarks/'$i'Slice.csp' > results/$i.txt
 done
