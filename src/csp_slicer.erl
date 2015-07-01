e2d2d1d92df6ff2c0b8e5dd2de5bf70786dd06c2
Salvador Tamarit <tamarit27@gmail.com>
2015-07-01 16:21:47 +0200

    small change for compatibility

--- src/csp_slicer.erl
+++ src/csp_slicer.erl
@@ -18,7 +18,7 @@ get_all_slices(G) ->
 					Sync0 = 
 						[V1 || {_,V1,_,"sync"} <- InfoEs0] 
 						++ [V2 ||{_,_,V2,"sync"} <- InfoOs0],
-					{lists:min([V|Sync0]), lists:usort([get_actual_vertex(G, VS) || VS <- [V|Sync0]])};
+					{lists:min([V|Sync0]), lists:usort([get_actual_vertex(G, VS)||VS <- [V|Sync0] ])};
 				_ -> 
 					[]
 		  end || V <- digraph:vertices(G) ],
