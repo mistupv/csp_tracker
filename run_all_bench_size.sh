b7a2d95648499fe925cbb5bffde5ae285bc46cc2
Salvador Tamarit <tamarit27@gmail.com>
2015-10-12 21:56:27 +0200

    Corrected script

--- run_all_bench_size.sh
+++ run_all_bench_size.sh
@@ -2,6 +2,6 @@
 
 array=("Loop" "ProdCons" "ReadWrite")
 for i in "${array[@]}"; do 
-    escript run_bench.sh 'benchmarks/'$i'.csp' > results/$i.txt
+    escript run_bench_size.sh 'benchmarks/'$i'.csp' > results/$i.txt
 done
 
