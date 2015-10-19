dacc90810fe25e9421b85b4732b30de28f7e2fc6
Salvador Tamarit <tamarit27@gmail.com>
2015-10-20 01:33:51 +0200

    benchmarking

--- run_all_bench_size.sh
+++ run_all_bench_size.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #array=("Loop" "ProdCons" "ReadWrite")
-array=("ReadWrite")
+array=("ReadWrite" "ProdCons")
 for i in "${array[@]}"; do 
     escript run_bench_size.sh 'benchmarks/'$i'.csp' > results/$i.txt
 done
