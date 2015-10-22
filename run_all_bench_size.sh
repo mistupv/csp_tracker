f738d59f72e995b2a2d590e9f208d979a4c0e25f
Salvador Tamarit <tamarit27@gmail.com>
2015-10-22 12:11:34 +0200

    benchmarking

--- run_all_bench_size.sh
+++ run_all_bench_size.sh
@@ -6,6 +6,7 @@ for i in "${array[@]}"; do
     escript run_bench_size.sh 'benchmarks/'$i'.csp' > results/$i.txt
 done
 
-#escript run_bench_size.sh 'benchmarks/ReadWrite.csp' > results/ReadWrite.txt
+# escript run_bench_size.sh 'benchmarks/ReadWrite.csp' > results/ReadWrite.txt
 # escript run_bench_size.sh 'benchmarks/Loop.csp' > results/Loop.txt
+# escript run_bench_size.sh 'benchmarks/ProdCons.csp' > results/ProdCons.txt
 
--- run_bench_size.sh
+++ run_bench_size.sh
@@ -7,7 +7,7 @@ main([String]) ->
 	io:format("Process limit: ~p\n",[erlang:system_info(process_limit)]),
 	csp_tracker_loader:load(),
 	File = list_to_atom(String),  
-	MiliSecList = lists:seq(100, 3600, 100),
-	[csp_bench:bench_no_latex(File,'MAIN',MiliSec,100) 
+	MiliSecList = lists:seq(53000, 60000, 1000),
+	[csp_bench:bench_no_latex(File,'MAIN',MiliSec,50) 
 	 || MiliSec <- MiliSecList],
 	ok.
