0a72e10beed495ec39bc3a24a626bdd88aac18c1
tamarit <tamarit27@gmail.com>
2017-01-24 18:42:19 +0100

    New service

--- src/printer.erl
+++ src/printer.erl
@@ -124,6 +124,14 @@ loop(Free,PrintInternals,LiveSaving,State) ->
 					Pid!{info_graph, {State, G}},
 					finish_computation()
 			end;
+		{info_graph_no_stop,Pid} ->
+			% Because it is only called when finished 
+			digraph!{get, self()},
+			receive
+				{digraph,G} ->
+					Pid!{info_graph, {State, G}},
+					loop(Free,PrintInternals,LiveSaving,State);
+			end;
 		{get_trace, Pid} ->
 			{_,_,Trace} = State,
 			Pid!{trace, Trace},
